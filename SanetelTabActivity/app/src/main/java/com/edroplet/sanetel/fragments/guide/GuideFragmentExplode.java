e94fbba8ca60b8c7347bf8cbb02a7e607d514855
qxx <edroplet@sina.com>
2018-01-12 11:10:57 +0800

    修改展开页面

--- SanetelTabActivity/app/src/main/java/com/edroplet/sanetel/fragments/guide/GuideFragmentExplode.java
+++ SanetelTabActivity/app/src/main/java/com/edroplet/sanetel/fragments/guide/GuideFragmentExplode.java
@@ -3,17 +3,19 @@ package com.edroplet.sanetel.fragments.guide;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.edroplet.sanetel.R;
 import com.edroplet.sanetel.beans.AntennaInfo;
 import com.edroplet.sanetel.beans.Protocol;
 import com.edroplet.sanetel.utils.PopDialog;
+import com.edroplet.sanetel.view.TimerFragment;
 import com.edroplet.sanetel.view.custom.CustomButton;
+import com.edroplet.sanetel.view.custom.CustomTextView;
 
 import butterknife.BindArray;
 import butterknife.BindView;
@@ -25,28 +27,25 @@ import butterknife.Unbinder;
  * 展开页面，只有一个设置展开
  */
 
-public class GuideFragmentExplode extends Fragment {
+public class GuideFragmentExplode extends TimerFragment {
     private int[] icons = {R.drawable.antenna_exploded, R.drawable.park, R.drawable.searching, R.drawable.recycle, R.drawable.folder};
-    public static GuideFragmentExplode newInstance(boolean showFirst, String firstLine, boolean showSecond,
-                                                   String secondLine, boolean showThird, String thirdLineStart,
-                                                   int icon, String buttonText, String thirdLineEnd) {
-        Bundle args = new Bundle();
-        GuideFragmentExplode fragment = new GuideFragmentExplode();
-        args.putBoolean(PopDialog.SHOW_FIRST,showFirst);
-        args.putString(PopDialog.FIRST, firstLine);
-        args.putBoolean(PopDialog.SHOW_SECOND,showSecond);
-        args.putString(PopDialog.SECOND, secondLine);
-        args.putBoolean(PopDialog.SHOW_THIRD,showThird);
-        args.putString(PopDialog.START, thirdLineStart);
-        args.putInt(PopDialog.ICON, icon);
-        args.putString(PopDialog.BUTTON_TEXT, buttonText);
-        args.putString(PopDialog.END, thirdLineEnd);
-        fragment.setArguments(args);
-        return fragment;
+    public static GuideFragmentExplode newInstance() {
+        return new GuideFragmentExplode();
     }
 
     @BindView(R.id.pop_dialog_third_button)
     CustomButton thirdButton;
+    @BindView(R.id.pop_dialog_tv_first)
+    CustomTextView tvFirstLine;
+    @BindView(R.id.pop_dialog_tv_second)
+    CustomTextView tvSecondLine;
+    @BindView(R.id.pop_dialog_tv_third_start)
+    CustomTextView tvThirdStart;
+    @BindView(R.id.pop_dialog_tv_third_end)
+    CustomTextView tvThirdEnd;
+
+    @BindView(R.id.pop_dialog_third)
+    LinearLayout thirdLine;
 
     @BindArray(R.array.antenna_state_array)
     String[] antennaStateArray;
@@ -82,22 +81,72 @@ public class GuideFragmentExplode extends Fragment {
         popDialog.setView(view);
         popDialog.setContext(getContext());
 
-        Bundle bundle = getArguments();
-        if (bundle != null) {
-            popDialog.setBundle(bundle);
-            popDialog.setSetFirstColor(true);
+        Bundle bundle = getBundles();
+        popDialog.setBundle(bundle);
+        popDialog.setSetFirstColor(true);
 
-            int icon = bundle.getInt(PopDialog.ICON, -1);
-            if (icon >= 0) {
-                popDialog.setDrawable(ContextCompat.getDrawable(getContext(),icons[icon]));
-            }
+        int icon = bundle.getInt(PopDialog.ICON, -1);
+        if (icon >= 0) {
+            popDialog.setDrawable(ContextCompat.getDrawable(getContext(),icons[icon]));
         }
         return popDialog.show();
+    }
+
+    @Override
+    public void doTimer() {
+        super.doTimer();
+        int antennaState = AntennaInfo.getAntennaState(getContext());
+        tvFirstLine.setVisibility(View.VISIBLE);
+        if (antennaState < antennaStateArray.length){
+            tvFirstLine.setText(String.format(getString(R.string.follow_me_antenna_first_line), antennaStateArray[antennaState]));
+        }
+        tvSecondLine.setVisibility(View.INVISIBLE);
+        thirdLine.setVisibility(View.INVISIBLE);
+        switch (antennaState){
+            case AntennaInfo.AntennaSearchSatellitesStatus.EXPLODED:
+                tvFirstLine.setText(String.format(getString(R.string.follow_me_antenna_first_line), antennaStateArray[antennaState]));
+                tvSecondLine.setVisibility(View.VISIBLE);
+                tvSecondLine.setText(getString(R.string.follow_me_antenna_state_explode_second_line));
+                thirdLine.setVisibility(View.VISIBLE);
+                tvThirdStart.setText(getString(R.string.follow_me_antenna_state_explode_third_start));
+                break;
+            case AntennaInfo.AntennaSearchSatellitesStatus.FOLDED:
+                thirdLine.setVisibility(View.VISIBLE);
+                tvThirdStart.setText(getString(R.string.follow_me_message_click));
+                tvThirdEnd.setText(getString(R.string.antenna_state_exploded));
+                thirdButton.setCompoundDrawables(ContextCompat.getDrawable(getContext(),icons[0]),null,null,null);
+                break;
+            case AntennaInfo.AntennaSearchSatellitesStatus.EXPLODING:
+                thirdLine.setVisibility(View.VISIBLE);
+                tvThirdStart.setText(getString(R.string.follow_me_message_click));
+                tvThirdEnd.setText(getString(R.string.antenna_state_paused));
+                thirdButton.setCompoundDrawables(ContextCompat.getDrawable(getContext(),icons[1]),null,null,null);
+                break;
+            case AntennaInfo.AntennaSearchSatellitesStatus.ABNORMAL:
+            case AntennaInfo.AntennaSearchSatellitesStatus.PAUSE:
+            case AntennaInfo.AntennaSearchSatellitesStatus.SEARCHING:
+            case AntennaInfo.AntennaSearchSatellitesStatus.RECYCLED:
+                break;
+        }
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
         if (unbinder != null)  unbinder.unbind();
+    }
+
+    Bundle getBundles(){
+        Bundle args = new Bundle();
+        args.putBoolean(PopDialog.SHOW_FIRST,true);
+        args.putString(PopDialog.FIRST, String.format(getString(R.string.follow_me_antenna_first_line), antennaStateArray[antennaState]));
+        args.putBoolean(PopDialog.SHOW_SECOND,true);
+        args.putString(PopDialog.SECOND, getString(R.string.follow_me_antenna_state_explode_second_line));
+        args.putBoolean(PopDialog.SHOW_THIRD,true);
+        args.putString(PopDialog.START, getString(R.string.follow_me_antenna_state_explode_third_start));
+        args.putInt(PopDialog.ICON, -1);
+        args.putString(PopDialog.BUTTON_TEXT, null);
+        args.putString(PopDialog.END, null);
+        return args;
     }
 }
